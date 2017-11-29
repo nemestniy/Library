e45169d94976fc745845a5032f6337b62ee4b37b
Max <maxonchik174@gmail.com>
2017-11-29 22:38:08 +0300

    add a asynchronous inquiry

--- app/src/main/java/com/nemestniy/library/Translator.java
+++ app/src/main/java/com/nemestniy/library/Translator.java
@@ -1,6 +1,10 @@
 package com.nemestniy.library;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.StrictMode;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +22,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -47,20 +52,21 @@ public class Translator extends AppCompatActivity {
         tvTranslate = (TextView) findViewById(R.id.tvTranslate);
         getStr = (EditText) findViewById(R.id.editTranslate);
         translate = (Button) findViewById(R.id.translate);
+
         translate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HashMap<String, String> jsonMap = new HashMap<>();    // собираем запрос к API
+                HashMap<String, String> jsonMap = new HashMap<>();    // made inquiry
                 jsonMap.put("key", Api);
                 jsonMap.put("lang", "en-ru");
                 jsonMap.put("text", getStr.getText().toString());
 
-                Call<Object> call = inter.translate(jsonMap);         //делаем запрос
+                Call<Object> call = inter.translate(jsonMap);         //take inquiry
 
-                try {
+               /* try {                                                 //there we take synchronous inquiry
                     Response<Object> response = call.execute();
 
-                    Map<String, String> map = gson.fromJson(response.body().toString(), Map.class); // Здесь получаем ответ из Json в Gson
+                    Map<String, String> map = gson.fromJson(response.body().toString(), Map.class); // there we get callback in Gson
 
                     for(Map.Entry ent : map.entrySet()) {
                         if(ent.getKey().equals("text")){
@@ -74,9 +80,34 @@ public class Translator extends AppCompatActivity {
                     Toast.makeText(Translator.this, "Не работает", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
+        }); */
+              call.enqueue(new Callback<Object>() {         //there we take asynchronous inquiry
+                  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                  @Override
+                  public void onResponse(Call<Object> call, Response<Object> response) {
+                      if(response.body() != null) {
+                          Map<String, String> map = gson.fromJson(response.body().toString(), Map.class);
+                          for (Map.Entry e : map.entrySet()) {
+                              System.out.println(e.getValue().toString());
+                              if (e.getKey().equals("text")) {
+                                  tvTranslate.setText(e.getValue().toString());
+                                  translate.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                              }
+                          }
+                      }
+                  }
+
+                  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                  @Override
+                  public void onFailure(Call<Object> call, Throwable t) {
+                        Toast.makeText(Translator.this, "Don`t work", Toast.LENGTH_SHORT).show();
+                        translate.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+                  }
+              });
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
     }
+    });
+}
 }
